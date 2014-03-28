ce80c63024a8e029a3d1d62f8472cb3343688668
Nehemiah I. Dacres <vivacarlie@gmail.com>
2014-03-27 23:35:01 -0500

    removing jquery, replacing with native XPath parse

--- action.js
+++ action.js
@@ -1,41 +1,26 @@
-
+// parse rss sans jquery
+/* jshint esnext: true */
 function pull(feedurl, containerID, maxItems = 8) {
     //feedurl: string, the url of the rss feed
     //containerID: string, the id of the container element, should be a flow element
     //maxItems: integer, the maximum number of items to display
-   var container = $(containerID);
-   function success(xml, textStatus, jqXHR) {  
-        var xmlDoc = $.parseXML(xml)
-        xmlDoc.find("item").slice(0,maxItems).each(function() { 
-                $(containerID).append("<li></li>").append($(this).find("title").text()).append("<a></a>").append($(this).)
-               //http://codeforbrowser.com/blog/parse-an-rss-feed-with-jquery/ 
-                anchor.setAttribute("href",entry.link)
-                anchor.appendChild(document.createTextNode(entry.title))
-            div.appendChild(anchor)
-            container.appendChild(div);
+    //
+    //var container = document.getElementById(container);
+    
+    //req.open("GET","http://www.siliconera.com/feed/",true)
+    var oReq = new XMLHttpRequest( );
+    oReq.open("GET",feedurl,false);
+    oReq.onload=function(container ){ 
+        var theXML = oReq.responseXML;
+        var items = theXML.evaluate("//item",theXML,null, XPathResult.ORDERED_NODE_ITERATOR_TYPE , null); 
+        //items is an itterator that retuns <item> elements of the RSS Feed.
+        var next = items.iterateNext(); 
+        while (next){ //WARNING, this will iterate over ENTIRE THING!
+            //iterate over item nodes >> <a href="node.link" >node.title</a>
+            //put description in popup 
+        }
 
 
-
-        } )
-	      }
-  }
-    var xhr = $.get(feedurl) 
-});
+    };
+    oReq.send();
 }
-function initialize() {
-	var slashdot = new google.feeds.Feed("http://rss.slashdot.org/Slashdot/slashdot")
-	//var siliconera= new google.feeds.Feed("http://feeds.feedburner.com/siliconera/MkOc?format=xml")
-	var theH = new google.feeds.Feed("http://www.h-online.com/grand-atom.xml")
-	var kotaku= new google.feeds.Feed("http://feeds.gawker.com/kotaku/full")
-	var Phoronix = new google.feeds.Feed("http://feeds.feedburner.com/Phoronix")
-	loadRSS(Phoronix, "phoronix");
-	loadRSS(theH, "the-h");
-	loadRSS(kotaku, "kotaku");
-	loadRSS(slashdot, "slashdot");
-}
-$(".refresh").click(function(){
-  var feedID = $(this).data("feed");
-  if (feedID == "phoronix") {loadRSS(Phoronix,"phoronix")}
-  else if (feedID == "the-h") {loadRSS(theH,"the-h")}
-});
-})
