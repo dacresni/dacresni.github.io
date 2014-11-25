7edcfa1bf6ae65e4843c82a508685253d6b8b525
Nehemiah I. Dacres <vivacarlie@gmail.com>
2014-11-24 21:06:04 -0800

    changed parameter list

--- action.js
+++ action.js
@@ -1,6 +1,6 @@
 // parse rss sans jquery
 /* jshint esnext: true */
-function putItemIntoContaner(item, container){ 
+function putItemIntoContaner(item, container){
     var titleElement = item.getElementsByTagName("title")[0];
     var linkElement= item.getElementsByTagName("link")[0];
     var framgent = document.createDocumentFragment();
@@ -13,26 +13,27 @@ function putItemIntoContaner(item, container){
     var container = document.getElementsById(container);
     container.appendChild(fragment);
     //iterate over item nodes >> <a href="node.link" >node.title</a>
-    //put description in popup 
-   
+    //put description in popup
+
 }
 
-function pull(feedURL, containerID, maxItems = 8) {
+function pull(feedURL, containerID ) {
+    var maxitems = 8 ;
     //feedURL: string, the url of the rss feed
     //containerID: string, the id of the container element, should be a flow element
     //maxItems: integer, the maximum number of items to display
     //
-    
+
     //req.open("GET","http://www.siliconera.com/feed/",true)
     var oReq = new XMLHttpRequest( );
     oReq.open("GET",feedURL,false);
-    oReq.onload=function(){ 
+    oReq.onload=function(){
         var theXML = oReq.responseXML;
-        var items = theXML.evaluate("//item",theXML,null, XPathResult.ORDERED_NODE_ITERATOR_TYPE , null); 
+        var items = theXML.evaluate("//item",theXML,null, XPathResult.ORDERED_NODE_ITERATOR_TYPE , null);
         //items is an itterator that retuns <item> elements of the RSS Feed.
-        var next = items.iterateNext(); 
+        var next = items.iterateNext();
         var i=0 ;
-        while (next & i < maxItems){ //WARNING, this will iterate over ENTIRE THING
+        while (next & i < maxItems){ //WARNING, this could iterate over ENTIRE THING
             putItemIntoContaner(next, containerID);
             next = items.iterateNext();
             i++;
