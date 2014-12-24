b4ec252dfa1a81f5459ec38f4936ad13257e9607
Nehemiah I. Dacres <vivacarlie@gmail.com>
2014-12-23 22:22:00 -0600

    added app manifest filek,

--- action.js
+++ action.js
@@ -41,10 +41,6 @@ function pull(feedURL, containerID ) {
     };
     oReq.send();
 }
-var Marketplace = new google.feeds.Feed("http://www.marketplace.org/latest-stories/long-feed.xml");
-var kotaku = new google.feeds.Feed("http://www.siliconera.com/feed/");
-var theVerge = new google.feeds.Feed("http://www.theverge.com/rss/index.xml");
-var linuxNews = new google.feeds.Feed("http://www.linux.com/feeds/original-content");
 
 var feeds = new Map([
     ["Marketplace","http://www.marketplace.org/latest-stories/long-feed.xml"],
