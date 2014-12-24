fe13ad91cbdbed3072ea467a0c343c7e92701c72
Nehemiah I. Dacres <vivacarlie@gmail.com>
2014-12-23 21:40:51 -0600

    brackets not braces for iterable kv array

--- action.js
+++ action.js
@@ -46,11 +46,11 @@ var kotaku = new google.feeds.Feed("http://www.siliconera.com/feed/");
 var theVerge = new google.feeds.Feed("http://www.theverge.com/rss/index.xml");
 var linuxNews = new google.feeds.Feed("http://www.linux.com/feeds/original-content");
 
-var feeds = new Map({
+var feeds = new Map([
     ["Marketplace","http://www.marketplace.org/latest-stories/long-feed.xml"],
     ["Siliconera","http://www.siliconera.com/feed/"],
     ["theVerge","http://www.theverge.com/rss/index.xml"],
     ["linuxNews","http://www.linux.com/feeds/original-content"]
-});
+]);
 
 feeds.forEach(pull)
