6760b1a47de919882afd2894d8d79d1bcd460ab4
Nehemiah I. Dacres <vivacarlie@gmail.com>
2019-01-02 13:33:43 -0600

    conteniuing along with vue tutorial

--- vuepart.js
+++ vuepart.js
@@ -1,12 +1,15 @@
-var slashdot = new Vue({
-  el: '#slashdot',
-  data: {
-    feed: [
-        { title:"first"}, 
-        { title:"second"}, 
-        { title:"third"} 
-    ]
-  }
+Vue.component('feed-item', {
+    props: ['feed'],
+    template: '<li><a v-bind:href="url"> {{feed.headline }} </a> ></li>'
 })
 
-
+var rssfeed = new Vue( {
+    el: "#slashdot",
+    data: { 
+        articles:[
+            {id:0, headline: "slashdot", url: "https://slashdot.org"},
+            {id:1, headline: "phoronix", url: "https://www.phoronix.com"},
+            {id:2, headline: "slashdot", url: "http://www.siliconera.com/"}
+        ]
+    }
+})
