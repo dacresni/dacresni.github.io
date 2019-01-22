d69798caa8fa8747e8c15b22aec83c0b02825978
Nehemiah I. Dacres <vivacarlie@gmail.com>
2019-01-22 11:38:37 -0600

    wip

--- vuepart.js
+++ vuepart.js
@@ -1,15 +1,15 @@
 Vue.component('feed-item', {
     props: ['feed'],
-    template: '<li><a v-bind:href="url"> {{feed.headline }} </a> ></li>'
+    template: '<li><a v-bind:href="feed.url" > {{feed.headline }} </a> </li>'
 })
+//{id:0, headline: "slashdot", url: "https://slashdot.org"}, {id:1, headline: "phoronix", url: "https://www.phoronix.com"}, {id:2, headline: "slashdot", url: "http://www.siliconera.com/"}
 
 var rssfeed = new Vue( {
     el: "#slashdot",
     data: { 
-        articles:[
-            {id:0, headline: "slashdot", url: "https://slashdot.org"},
-            {id:1, headline: "phoronix", url: "https://www.phoronix.com"},
-            {id:2, headline: "slashdot", url: "http://www.siliconera.com/"}
-        ]
-    }
+        articles:[ ]
+    },
+    created: function() { 
+        fetch("https://feed.offtopical.net/", { method: "GET",mode:"cors"}).then(function(response){ Console.log(response.text())});
+    } 
 })
