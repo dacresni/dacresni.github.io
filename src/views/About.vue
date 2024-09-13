e6007c4413e6476ff5b3cbe86109acda8bb0b26c
Nehemiah I. Dacres <Nehemiah.Dacres@ci.stpaul.mn.us>
2024-09-13 11:38:11 -0500

    upgrade nvm, added to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,5 +4,12 @@
     <p>
       Welcome to my website. It is a work in progress using <a href="https://vuejs.org/">Vue.js</a>
     </p>
+    <ul> 
+      <li> <a  href ="https://www.linkedin.com/in/nehemiah-dacres-24493a9/" >Linkedin </a> </li>
+      <li> <a  href ="https://www.threads.net/@dacresni"> Threads </a> </li>
+      <li> <a  href ="https://www.youtube.com/@dacresni"> YouTube </a> </li>
+      <li> <a  href ="https://www.instagram.com/dacresni/"> Instagram </a></li>
+      <li> <a  href ="https://mastodon.social/@dacresni"> Mastodon </a></li>
+    </ul>
   </div>
 </template>
